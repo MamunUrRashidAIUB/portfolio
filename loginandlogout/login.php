61d89bddaadff8e7f34f6221fb51cf37019784b6
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-02 05:07:02 +0600

    style change in login

--- loginandlogout/login.php
+++ loginandlogout/login.php
@@ -25,6 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="POST" action="">
