76b1380cfcfdb7b88dfb7f36fe5473ee3c3472f8
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-02 06:33:45 +0600

    redesign the code for more beautifull view

--- index.php
+++ index.php
@@ -53,39 +53,7 @@ $result = $conn->query($sql);
                 <?php endif; ?>
             </nav>
         </header>
-
-        <h1>Successful Front-end Development</h1>
-        
-        <div class="intro">
-            <p>Hi, I'm Rashid, a freelance Front-end Developer with 1 year commercial experience creating successful websites.</p>
-        </div>
-
-        <div class="services">
-            <div class="service">
-                <img src="images/25.png" alt="Icon representing front-end development services">
-                <h2>Front-end development</h2>
-                <p>Responsive websites built for an optimal user experience that achieves your business goals.</p>
-            </div>
-            <div class="service">
-                <img src="images/12.png" alt="Icon representing Flutter app development">
-                <h2>Flutter developer</h2>
-                <p>Manage apps using the Flutter framework.</p>
-            </div>
-            <div class="service">
-                <img src="images/23.png" alt="Icon representing interface design with Figma">
-                <h2>Figma</h2>
-                <p>Collaborative web application for interface design, with additional offline features enabled by desktop applications for macOS and Windows.</p>
-            </div>
-        </div>
-
-        <div class="projects">
-            <h2>My Work and Projects</h2>
-            <!-- Add project content here -->
-        </div>
-        
-        <!-- WHO AM I Section -->
-<div class="who-am-i-section" id="about">
-    <h2>WHO AM I?</h2>
+        <div class="who-am-i-section" id="about">
     <div class="who-am-i-container">
         <div class="who-am-i-image">
             <img src="images/55.jpeg" alt="Image of Rashid">
@@ -95,6 +63,13 @@ $result = $conn->query($sql);
         </div>
     </div>
 </div>
+        <div class="projects">
+            <h2>My Work and Projects</h2>
+            <!-- Add project content here -->
+        </div>
+        
+       
+
 
 
         <div class="projects" id="testimonials">
